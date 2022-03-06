c217555104c70411dfb5ee5812b3450f292ef7a1
HARSH VISHWAKARMA <harshvishwakarma111@gmail.com>
2022-03-06 10:49:43 +0530

    added _document to resolve next_js styled c issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
-
+import { ThirdwebSDK } from "@3rdweb/sdk";
+import { ThirdwebProvider } from "@3rdweb/react";
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+import "../styles/globals.css";
+const supportedChainIds = [4];
+const connectors = {
+  injected: {},
+};
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
